d52d16156fdf1304dd86d346c2d3fde05b067a32
Michael <michaellunn@git.edstem.org>
2024-09-09 19:06:18 +1000

    Test pipeline

--- src/main/java/controller/ConnServlet.java
+++ src/main/java/controller/ConnServlet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.servlet.ServletException;
+import javax.servlet.ServletException; 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
