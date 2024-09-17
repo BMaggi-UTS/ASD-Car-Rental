1ba23a73ae2a5e368ea7cb78345c726eb65518df
Callum <callum@Callums-MacBook-Pro.local>
2024-09-17 13:59:09 +1000

    update db user pass

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.sql.Connection;
 public abstract class DB {   
+<<<<<<< HEAD
     protected String URL = "jdbc:mysql://3.27.200.202:3306/";//replace this string with your jdbc:derby local host url   
     protected String db = "CarRental-ASD";//name of the database   
     protected String dbuser = "root";//db root user   
@@ -9,3 +10,13 @@ public abstract class DB {
     protected String driver = "com.mysql.cj.jdbc.Driver"; //jdbc client driver
     protected Connection conn; //connection null-instance to be initialized in sub-classes
 }
+=======
+
+protected String URL = "jdbc:mysql://13.55.60.202:3306/";//replace this string with your jdbc:derby local host url   
+protected String db = "CarRental-ASD";//name of the database   
+protected String dbuser = "wy5jR63bAEYi1G~T";//db root user   
+protected String dbpass = "6JcL1ECgLzQnmH75"; //db root password   
+protected String driver = "com.mysql.cj.jdbc.Driver"; //jdbc client driver
+protected Connection conn; //connection null-instance to be initialized in sub-classes
+}
+>>>>>>> 685df5e (update db user pass)
