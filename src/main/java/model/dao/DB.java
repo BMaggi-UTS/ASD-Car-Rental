f221538a21409af9349f8eb68a76dc8ad6cf036a
Callum <callum@Callums-MacBook-Pro.local>
2024-09-17 14:04:45 +1000

    update db user pass

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -14,7 +14,7 @@ public abstract class DB {
 
 protected String URL = "jdbc:mysql://13.55.60.202:3306/";//replace this string with your jdbc:derby local host url   
 protected String db = "CarRental-ASD";//name of the database   
-protected String dbuser = "wy5jR63bAEYi1G~T";//db root user   
+protected String dbuser = "wy5jR63bAEYi1GxT";//db root user   
 protected String dbpass = "6JcL1ECgLzQnmH75"; //db root password   
 protected String driver = "com.mysql.cj.jdbc.Driver"; //jdbc client driver
 protected Connection conn; //connection null-instance to be initialized in sub-classes
