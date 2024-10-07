eff18266720f4fc34ef24477b00911c370e8a7ed
Michael <michaellunn@git.edstem.org>
2024-10-07 22:00:46 +0300

    test

--- src/main/java/model/dao/DBConnector.java
+++ src/main/java/model/dao/DBConnector.java
@@ -4,23 +4,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class DBConnector extends DB {
 
-	Connection conn;
-
 	public DBConnector() throws ClassNotFoundException, SQLException {
-		Class.forName(driver);
+	Class.forName(driver);
 
-		Properties dbProperties = new Properties();
+	Properties dbProperties = new Properties();
 		dbProperties.put("user", dbuser);
 		dbProperties.put("password", dbpass);
 		dbProperties.put("allowPublicKeyRetrieval", "true");
 		dbProperties.put("useSSL", "false");
 
-		conn = DriverManager.getConnection(URL + db, dbProperties);
+		try {
+			conn = DriverManager.getConnection(URL + db, dbProperties);
+		} catch (SQLException ex) {
+			Logger.getLogger(DBConnector.class.getName()).log(Level.SEVERE, null, ex);
+			System.out.println(ex);
+      		throw ex;
+		}
 	}
-	
 
 	public Connection openConnection(){
 		return this.conn;
