56fb261a820590ce15599ad8f65dfc4eaca634a4
Michael <michaellunn@git.edstem.org>
2024-11-01 14:28:33 +1100

    Added new db dump

--- db/Dump20241101.sql
+++ db/Dump20241101.sql
@@ -29,7 +29,7 @@ CREATE TABLE `Availability` (
   `Availability_Finish_Date` date NOT NULL,
   `Car_ID` int(11) NOT NULL,
   PRIMARY KEY (`Availability_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=99994 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=100733 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `Availability` (
 
 LOCK TABLES `Availability` WRITE;
 /*!40000 ALTER TABLE `Availability` DISABLE KEYS */;
-INSERT INTO `Availability` VALUES (1,21,'2024-12-25','2025-10-26',2),(2,12,'2025-10-01','2024-10-05',22),(3,3,'2024-10-11','2024-10-15',3),(4,4,'2024-10-01','2024-10-05',4),(5,5,'2024-10-06','2024-10-10',5),(6,6,'2024-10-11','2024-10-15',6),(7,7,'2024-10-01','2024-10-05',7),(8,8,'2024-10-06','2024-10-10',8),(9,9,'2024-10-11','2024-10-15',9),(10,10,'2024-10-01','2024-10-15',10);
+INSERT INTO `Availability` VALUES (1,2,'2024-12-02','2025-10-26',2),(2,3,'2025-10-01','2024-10-05',22),(3,3,'2024-10-11','2024-10-15',23),(4,1,'2024-10-01','2024-10-05',4),(5,1,'2024-10-06','2024-10-10',45),(6,6,'2024-10-11','2024-10-15',1),(7,7,'2024-10-01','2024-10-05',2),(8,8,'2024-10-06','2024-10-10',8),(9,9,'2024-10-11','2024-10-15',9),(10,10,'2024-10-01','2024-10-15',10),(99999,100977,'2024-10-10','2024-10-15',14),(100310,101798,'2024-10-10','2024-10-15',13),(100686,104130,'2024-10-10','2024-10-15',13),(100704,104328,'2024-10-29','2024-11-03',20),(100707,104342,'2024-10-29','2024-11-03',14),(100709,104356,'2024-10-29','2024-11-02',13),(100710,104357,'2024-10-29','2024-11-23',14),(100711,104358,'2024-10-29','2024-12-07',14);
 /*!40000 ALTER TABLE `Availability` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,7 +69,7 @@ CREATE TABLE `Car` (
   PRIMARY KEY (`Car_ID`),
   KEY `Car_Location_FK` (`Location_ID`),
   CONSTRAINT `Car_Location_FK` FOREIGN KEY (`Location_ID`) REFERENCES `Location` (`Location_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=100044 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=100094 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,7 +78,7 @@ CREATE TABLE `Car` (
 
 LOCK TABLES `Car` WRITE;
 /*!40000 ALTER TABLE `Car` DISABLE KEYS */;
-INSERT INTO `Car` VALUES (13,'MG','3',NULL,20000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/mg-mg3-my19-index.png','M','P',5,'Hatch','Fuel Efficient',25000,1,1,4,1),(14,'Toyota','Corolla','GXL',15000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/toyota-corolla-my19-index-%281%29.png','A','H',5,'Hatch','Handles Well',30000,1,3,5,1),(15,'Toyota','Camry','LE',50000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/toyota-camry-my21-index-1.png','A','P',5,'Sedan','Reliable and spacious',20000,18000,1,5,2),(16,'Honda','Civic','EX',30000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/honda-civic-index.png','M','P',5,'Sedan','Fuel efficient and compact',18000,16000,1,5,5),(17,'Ford','Escape','Titanium',40000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/ford-escape-my21-index-1.png','A','P',5,'SUV','Adventure-ready',25000,22000,1,4,1),(18,'Holden','Cruze','LS',60000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Holden-Cruze.png','A','P',5,'Sedan','Comfortable and smooth',22000,20000,1,4,4),(19,'BMW','3 Series','330i',35000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/bmw-3-series-index.png','A','P',5,'Sedan','Luxury and performance',35000,32000,1,5,3),(20,'Audi','Q5','Premium Plus',45000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/audi-q5-my21-index-1.png','A','P',5,'SUV','Elegant and powerful',40000,37000,1,5,1),(21,'Nissan','Pulsar','ST',25000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Nissan-Pulsar_0.png','A','P',5,'Sedan','Stylish and efficient',19000,17000,1,5,4),(22,'Hyundai','Elantra','SE',20000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/Elantra-Active-index-1200x800-%281%29.png','M','P',5,'Sedan','Affordable and reliable',16000,14000,1,4,1),(23,'Subaru','Outback','Limited',55000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/subaru-outback-my21-index-01.png','A','P',5,'SUV','Versatile and rugged',28000,25000,1,5,2),(24,'Mercedes-Benz','C-Class','C300',30000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/merc%20c300%20small_0.png','A','P',5,'Sedan','Luxury and class',42000,40000,1,5,7);
+INSERT INTO `Car` VALUES (13,'MGa','a',NULL,20000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/mg-mg3-my19-index.png','A','P',5,'Hatch','Fuel Efficient',25000,1,1,4,1),(14,'Toyota','Corolla',NULL,15000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/toyota-corolla-my19-index-%281%29.png','A','H',5,'Hatch','Handles Well',30000,1,3,5,1),(15,'Toyota','Camry',NULL,50000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/toyota-camry-my21-index-1.png','A','P',5,'Sedan','Reliable and spacious',20000,18000,1,5,2),(16,'Hondaa','Civic',NULL,30000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/honda-civic-index.png','A','P',5,'Sedan','Fuel efficient and compact',18000,16000,1,5,5),(17,'Ford','Escape',NULL,40000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/ford-escape-my21-index-1.png','A','P',5,'SUV','Adventure-ready',25000,22000,1,4,1),(18,'Holden','Cruze',NULL,60000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Holden-Cruze.png','A','P',5,'Sedan','Comfortable and smooth',22000,20000,1,4,4),(19,'BMW','3 Series',NULL,35000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/bmw-3-series-index.png','A','P',5,'Sedan','Luxury and performance',35000,32000,1,5,3),(20,'Audi','Q5',NULL,45000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/audi-q5-my21-index-1.png','A','P',5,'SUV','Elegant and powerful',40000,37000,1,5,1),(21,'Nissan','Pulsar',NULL,25000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Nissan-Pulsar_0.png','A','P',5,'Sedan','Stylish and efficient',19000,17000,1,5,4),(22,'Hyundai','Elantra',NULL,20000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/Elantra-Active-index-1200x800-%281%29.png','M','P',5,'Sedan','Affordable and reliable',16000,14000,1,4,1),(23,'Subaru','Outback',NULL,55000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/subaru-outback-my21-index-01.png','A','P',5,'SUV','Versatile and rugged',28000,25000,1,5,2),(24,'Mercedes-Benz','C-Class',NULL,30000,'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/merc%20c300%20small_0.png','A','P',5,'Sedan','Luxury and class',42000,40000,1,5,7),(25,'bmwe','a',NULL,1,'1','a','m',1,'n','u',1,1,1,1,1);
 /*!40000 ALTER TABLE `Car` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +133,7 @@ CREATE TABLE `Orders` (
   PRIMARY KEY (`Order_ID`),
   KEY `User_ID` (`User_ID`),
   KEY `Car_ID` (`Car_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=104632 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,8 +142,39 @@ CREATE TABLE `Orders` (
 
 LOCK TABLES `Orders` WRITE;
 /*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
-INSERT INTO `Orders` VALUES (163,3,13,'2024-10-09 03:35:55','2024-10-09','2024-10-10',20000,0,'888888','20.00','200','220.00');
+INSERT INTO `Orders` VALUES (104619,3,13,'2024-10-09 10:00:00','2024-10-10','2024-10-15',1000,1200,'123456','50.00','200.00','250.00');
 /*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Payments`
+--
+
+DROP TABLE IF EXISTS `Payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Payments` (
+  `Payment_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Order_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Name_on_Card` varchar(100) NOT NULL,
+  `Card_Number` varchar(16) NOT NULL,
+  `Expiry_Date` varchar(7) NOT NULL,
+  `CVV` varchar(4) NOT NULL,
+  PRIMARY KEY (`Payment_ID`),
+  KEY `Order_ID` (`Order_ID`),
+  KEY `User_ID` (`User_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1635 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Payments`
+--
+
+LOCK TABLES `Payments` WRITE;
+/*!40000 ALTER TABLE `Payments` DISABLE KEYS */;
+INSERT INTO `Payments` VALUES (1634,13,3,'John Doe','4111111111111111','2025-10','123');
+/*!40000 ALTER TABLE `Payments` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -187,7 +218,7 @@ CREATE TABLE `Suppliers` (
   `Contact_Phone` varchar(20) DEFAULT NULL,
   `Image_Path` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`Supplier_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=153 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6304 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,7 +254,7 @@ CREATE TABLE `User_Roles` (
 
 LOCK TABLES `User_Roles` WRITE;
 /*!40000 ALTER TABLE `User_Roles` DISABLE KEYS */;
-INSERT INTO `User_Roles` VALUES (1,3),(2,2),(3,1),(5,1),(396,1),(553,1),(993,1);
+INSERT INTO `User_Roles` VALUES (1,3),(2,2),(3,1),(5,1),(396,1),(553,1),(993,1),(3878,1);
 /*!40000 ALTER TABLE `User_Roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -246,7 +277,7 @@ CREATE TABLE `Users` (
   PRIMARY KEY (`User_ID`),
   UNIQUE KEY `User_email` (`User_Email`),
   UNIQUE KEY `User_Email_2` (`User_Email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1930 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15185 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -255,7 +286,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'Michael','Bunn','Micky','m.lunn54@gmail.com','0411222222','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','2001-08-20'),(2,'Hulk','Hogan','Hulk','h.hogan@gmail.com','0400000001','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','1996-08-05'),(3,'Jeff','Bezos','Jeff','j.bezos@gmail.com','0400000002','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','1986-11-07'),(4,'Michael','Lunn',NULL,'m.lunn55@gmail.com','0413525166','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2024-08-27'),(5,'Michael','Lunn',NULL,'m.lunn4@gmail.com','0413525166','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2024-08-28'),(396,'Test','Test',NULL,'test@test.com','0412345678','b1c788abac15390de987ad17b65ac73c9b475d428a51f245c645a442fddd078b','2000-10-25'),(553,'Bob','Patterson',NULL,'bob@gmail.com','0412822222','81b637d8fcd2c6da6359e6963113a1170de795e4b725b84d1e0b4cfd9ec58ce9','2024-09-30'),(993,'Bob','Hawke','Bob','bobhawke@gmail.com','0412345678','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','2000-02-23');
+INSERT INTO `Users` VALUES (1,'Michael','Lunn','Micky','m.lunn54@gmail.com','0411222222','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','2001-08-20'),(2,'Hulk','Hogan','Hulk','h.hogan@gmail.com','0400000001','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','1996-08-05'),(3,'Jeff','Bezos','Jeff','j.bezos@gmail.com','0400000002','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','1986-11-07'),(4,'Michael','Lunn',NULL,'m.lunn55@gmail.com','0413525166','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2024-08-27'),(5,'Michael','Lunn',NULL,'m.lunn4@gmail.com','0413525166','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2024-08-28'),(396,'Test','Test',NULL,'test@test.com','0412345678','b1c788abac15390de987ad17b65ac73c9b475d428a51f245c645a442fddd078b','2000-10-25'),(553,'Bob','Patterson',NULL,'bob@gmail.com','0412822222','81b637d8fcd2c6da6359e6963113a1170de795e4b725b84d1e0b4cfd9ec58ce9','2024-09-30'),(993,'Bob','Hawke','Bob','bobhawke@gmail.com','0412345678','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8','2000-02-23'),(3878,'Bradley','Madgwick',NULL,'bradmaggi9@gmail.com','0477555920','61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4','2024-10-22');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -268,4 +299,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-08 21:28:42
+-- Dump completed on 2024-11-01 14:27:18
